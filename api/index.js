05ee706fc294ee41fcf534ab66ec9dfc762b37dc
Luis Gustavo B. Santiago <74880596+Luis-f3lps@users.noreply.github.com>
2026-04-30 10:01:03 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -76,9 +76,7 @@ app.get('/index', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
-app.get('/admin', Autenticado, (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
-});
+
 
 app.get('/api/projetos', async (req, res) => {
     try {
